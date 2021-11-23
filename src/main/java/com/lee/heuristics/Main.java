0c9d8fa584bafe873f2e7925e05114f596cef904
lee <fowler.lee8@gmail.com>
2021-11-23 21:00:20 +0100

    iterative deepening depthfirstSearch

---
+++ src/main/java/com/lee/heuristics/Main.java
@@ -0,0 +1,26 @@
+package com.lee.heuristics;
+
+public class Main {
+
+   public static void main(String[] args) {
+
+      Node node1 = new Node("A");
+      Node node2 = new Node("B");
+      Node node3 = new Node("C");
+      Node node4 = new Node("D");
+      Node node5 = new Node("E");
+      Node node6 = new Node("F");
+      Node node7 = new Node("G");
+
+      node1.addNeighbour(node2);
+      node1.addNeighbour(node3);
+      node2.addNeighbour(node4);
+      node4.addNeighbour(node7);
+      node1.addNeighbour(node5);
+      node5.addNeighbour(node6);
+      node5.addNeighbour(node7);
+
+      Algorithm algorithms = new Algorithm(node7);
+      algorithms.runDeepeningSearch(node1);
+   }
+}
---
+++ src/main/java/com/lee/heuristics/Node.java
@@ -0,0 +1,51 @@
+package com.lee.heuristics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Node {
+
+   private String name;
+   private int depthLevel = 0;
+   private List<Node> adjacencyList;
+
+   public Node(String a) {
+      this.name = a;
+      this.adjacencyList = new ArrayList<>();
+   }
+
+   public void addNeighbour(Node n) {
+      this.adjacencyList.add(n);
+   }
+
+   @Override
+   public String toString() {
+      return this.name;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public int getDepthLevel() {
+      return depthLevel;
+   }
+
+   public void setDepthLevel(int depthLevel) {
+      this.depthLevel = depthLevel;
+   }
+
+   public List<Node> getAdjacencyList() {
+      return adjacencyList;
+   }
+
+   public void setAdjacencyList(List<Node> adjacencyList) {
+      this.adjacencyList = adjacencyList;
+   }
+}
